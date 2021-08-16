ceb6c232b7f259038e08e4fd2cf7c31b6f3c2bc5
Jose K James <josekjames5@gmail.com>
2021-08-16 12:41:24 +0530

    Backdrop scaffold in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,26 @@
 import 'package:backdrop/backdrop.dart';
 import 'package:flutter/material.dart';
 
-void main()=>runApp(MaterialApp(
-  home: Home()
-));
-
-class Home extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return BackdropScaffold(
-      appBar: BackdropAppBar(
-        title: Text('Backdrop'),
+void main() => runApp(
+      MaterialApp(
+        home: BackdropScaffold(
+          appBar: BackdropAppBar(
+            title: Text('Backdrop'),
+          ),
+          backLayer: const Center(child: Text('Hello')),
+          frontLayer: const Center(child: Text('Hello User')),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.airplanemode_on),
+            backgroundColor: Colors.redAccent,
+          ),
+        ),
       ),
+    );
 
-      backLayer: const Center(
-      child: Text('Hello')), 
-      frontLayer: const Center(
-      child: Text('Hello User')),
-    
-
-    //  FloatingActionButton: FloatingActionButton(
-    //   onPressed: (){},
-    //   child:Icon(Icons.airplanemode_on),
-    // backgroundColor: Colors.redAccent,
-    // ),
-  );
-  }
-}
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return;
+//   }
+// }
